8bef2c0d2b02fb42b0df9b8c19bcf2d24c35e45a
Kyle Coble <kwc57@cornell.edu>
2022-11-03 00:01:58 -0400

    Add camera straming to app

--- py/examples/virtual_joystick/main.py
+++ py/examples/virtual_joystick/main.py
@@ -1,6 +1,7 @@
 # Copyright (c) farm-ng, inc. All rights reserved.
 import argparse
 import asyncio
+import io
 import logging
 import os
 from typing import List
@@ -15,6 +16,9 @@ from farm_ng.canbus.packet import AmigaControlState
 from farm_ng.canbus.packet import AmigaRpdo1
 from farm_ng.canbus.packet import AmigaTpdo1
 from farm_ng.canbus.packet import DASHBOARD_NODE_ID
+from farm_ng.oak import oak_pb2
+from farm_ng.oak.camera_client import OakCameraClient
+from farm_ng.oak.camera_client import OakCameraClientConfig
 
 # Must come before kivy imports
 os.environ["KIVY_NO_ARGS"] = "1"
@@ -34,6 +38,7 @@ Config.set("kivy", "keyboard_mode", "systemanddock")
 from kivy.app import App  # noqa: E402
 from kivy.lang.builder import Builder  # noqa: E402
 from kivy.uix.widget import Widget  # noqa: E402
+from kivy.core.image import Image as CoreImage  # noqa: E402
 
 
 kv = """
@@ -50,7 +55,24 @@ BoxLayout:
             text: "angular rate:\\n" + str(app.amiga_rate) + " [rad/s]"
     VirtualJoystickWidget:
         id: joystick
-
+    TabbedPanel:
+        do_default_tab: False
+        TabbedPanelItem:
+            text: "Rgb"
+            Image:
+                id: rgb
+        TabbedPanelItem:
+            text: "Disparity"
+            Image:
+                id: disparity
+        TabbedPanelItem:
+            text: "Left"
+            Image:
+                id: left
+        TabbedPanelItem:
+            text: "Right"
+            Image:
+                id: right
 """
 
 
@@ -135,10 +157,12 @@ class VirtualPendantApp(App):
     amiga_rate = StringProperty()
     amiga_state = StringProperty()
 
-    def __init__(self, address: str, port: int) -> None:
+    def __init__(self, address: str, camera_port: int, canbus_port: int, stream_every_n: int) -> None:
         super().__init__()
         self.address = address
-        self.port = port
+        self.camera_port = camera_port
+        self.canbus_port = canbus_port
+        self.stream_every_n = stream_every_n
 
         self.tasks: List[asyncio.Task] = []
 
@@ -151,8 +175,11 @@ class VirtualPendantApp(App):
         self.max_speed = 1.0
         self.max_angular_rate = 1.0
 
-        self.client: CanbusClient
-        self.config: CanbusClientConfig
+        self.canbus_client: CanbusClient
+        self.canbus_config: CanbusClientConfig
+
+        self.camera_client: OakCameraClient
+        self.camera_config: OakCameraClientConfig
 
     def build(self):
         return Builder.load_string(kv)
@@ -179,16 +206,59 @@ class VirtualPendantApp(App):
             for task in self.tasks:
                 task.cancel()
 
-        # configure the camera client
-        self.config = CanbusClientConfig(address=self.address, port=self.port)
-        self.client = CanbusClient(self.config)
+        # configure the canbus client
+        self.canbus_config = CanbusClientConfig(address=self.address, port=self.canbus_port)
+        self.canbus_client = CanbusClient(self.canbus_config)
 
+        # configure the camera client
+        self.camera_config = OakCameraClientConfig(address=self.address, port=self.camera_port)
+        self.camera_client = OakCameraClient(self.camera_config)
+
+        # Drawing task(s)
         self.tasks.append(asyncio.ensure_future(self.draw_joystick()))
 
-        self.tasks.append(asyncio.ensure_future(self.stream_canbus(self.client)))
-        self.tasks.append(asyncio.ensure_future(self.send_can_msgs(self.client)))
-        self.tasks.append(asyncio.ensure_future(self.client.poll_service_state()))
+        # Canbus task(s)
+        self.tasks.append(asyncio.ensure_future(self.stream_canbus(self.canbus_client)))
+        self.tasks.append(asyncio.ensure_future(self.send_can_msgs(self.canbus_client)))
+        self.tasks.append(asyncio.ensure_future(self.canbus_client.poll_service_state()))
+
+        # Camera task(s)
+        self.tasks.append(asyncio.ensure_future(self.stream_camera(self.camera_client)))
+        self.tasks.append(asyncio.ensure_future(self.camera_client.poll_service_state()))
+
         return await asyncio.gather(run_wrapper(), *self.tasks)
+
+    async def stream_camera(self, client: OakCameraClient) -> None:
+        """This task listens to the camera client's stream and populates
+        the tabbed panel with all 4 image streams from the oak camera
+        """
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
+        response_stream = None
+
+        while True:
+            if client.state.value != oak_pb2.OakServiceState.RUNNING:
+                # start the streaming service
+                await client.start_service()
+                await asyncio.sleep(0.01)
+                continue
+            elif response_stream is None:
+                # get the streaming object
+                response_stream = client.stream_frames(every_n=self.stream_every_n)
+                await asyncio.sleep(0.01)
+                continue
+
+            response: oak_pb2.StreamFramesReply = await response_stream.read()
+            if response and response.status == oak_pb2.ReplyStatus.OK:
+                # get the sync frame
+                frame: oak_pb2.OakSyncFrame = response.frame
+
+                # get image and show
+                for view_name in ["rgb", "disparity", "left", "right"]:
+                    self.root.ids[view_name].texture = CoreImage(
+                        io.BytesIO(getattr(frame, view_name).image_data), ext="jpg"
+                    ).texture
 
     async def send_can_msgs(self, client: CanbusClient) -> None:
         """This task ensures the canbus client sendCanbusMessage method has the pose_generator it will use to send
@@ -250,12 +320,21 @@ class VirtualPendantApp(App):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="virtual-joystick")
     parser.add_argument("--address", type=str, default="localhost", help="The camera address")
-    parser.add_argument("--port", type=int, required=True, help="The grpc port where the canbus service is running.")
+    parser.add_argument(
+        "--camera-port", type=int, required=True, help="The grpc port where the camera service is running."
+    )
+    parser.add_argument(
+        "--canbus-port", type=int, required=True, help="The grpc port where the canbus service is running."
+    )
+    parser.add_argument("--stream-every-n", type=int, default=1, help="Streaming frequency (used to skip frames)")
+
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(VirtualPendantApp(args.address, args.port).app_func())
+        loop.run_until_complete(
+            VirtualPendantApp(args.address, args.camera_port, args.canbus_port, args.stream_every_n).app_func()
+        )
     except asyncio.CancelledError:
         pass
     loop.close()
