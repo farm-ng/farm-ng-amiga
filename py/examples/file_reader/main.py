99810783216b5bf5adcda3c8860bac034006acdc
edgar <edgar.riba@gmail.com>
2022-11-17 13:08:37 +0100

    update file reader example with latest farm-ng-amiga

--- py/examples/file_reader/main.py
+++ py/examples/file_reader/main.py
@@ -2,58 +2,68 @@
 # Copyright (c) farm-ng, inc. All rights reserved.
 import argparse
 from pathlib import Path
+from typing import List
 
 import cv2
 import numpy as np
+from farm_ng.core import event_pb2
 from farm_ng.core.events_file_reader import EventsFileReader
+from farm_ng.core.uri import uri_pb2
 from farm_ng.oak import oak_pb2
 
 
 def main(file_name: str) -> None:
+    # create the file reader
     reader = EventsFileReader(Path(file_name))
     assert reader.open()
-    reader.compute_offsets()
 
     # main window to visualize image
-    uris = reader.uris()
-    print(uris)
+    uris: List[uri_pb2.Uri] = reader.uris()
 
-    while True:
+    # choose the Uri stream to seek in file
+    uri: uri_pb2.Uri = uris[1]
+
+    num_events: int = reader.num_events(uri)
+    current_event: int = 0
+
+    while current_event < num_events - 1:
         # read frame by frame
-        event, sample = reader.read()
-        if not event:
-            break
-        if event.name != "OakDataSample":
-            continue
+        # get the event and offset
+        event: event_pb2.Event
+        offset: int
+        event, offset = reader.get_event(uri, current_event)
+
+        # seek and parse the message
+        sample: oak_pb2.OakDataSample
+        sample = reader.read_message(event, offset)
         frame: oak_pb2.OakSyncFrame = sample.frame
-        if frame is None:
-            break
 
         # cast image data bytes to numpy and decode
         # NOTE: explore frame.[rgb, disparity, left, right]
-        depth = cv2.imdecode(np.frombuffer(frame.disparity.image_data, dtype="uint8"), cv2.IMREAD_GRAYSCALE)
+        disparity = cv2.imdecode(np.frombuffer(frame.disparity.image_data, dtype="uint8"), cv2.IMREAD_GRAYSCALE)
         rgb = cv2.imdecode(np.frombuffer(frame.rgb.image_data, dtype="uint8"), cv2.IMREAD_UNCHANGED)
 
         # visualize the image
-        #
-        depth_color = cv2.applyColorMap(depth * 2, cv2.COLORMAP_HOT)
+        disparity_color = cv2.applyColorMap(disparity * 2, cv2.COLORMAP_HOT)
 
         rgb_window_name = "rgb:" + event.uri.query
-        depth_window_name = "depth:" + event.uri.query
+        disparity_window_name = "depth:" + event.uri.query
 
-        cv2.namedWindow(depth_window_name, cv2.WINDOW_NORMAL)
+        # we use opencv for convenience, use kivy, pangolin or you preferred viz tool :)
+        cv2.namedWindow(disparity_window_name, cv2.WINDOW_NORMAL)
         cv2.namedWindow(rgb_window_name, cv2.WINDOW_NORMAL)
 
-        cv2.imshow(depth_window_name, depth_color)
+        cv2.imshow(disparity_window_name, disparity_color)
         cv2.imshow(rgb_window_name, rgb)
         cv2.waitKey(30)
 
-    reader.close()
+        current_event += 1
+
+    assert reader.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Event file reader example.")
     parser.add_argument("--file-name", type=str, required=True, help="Path to the `events.log` file.")
     args = parser.parse_args()
-
     main(args.file_name)
