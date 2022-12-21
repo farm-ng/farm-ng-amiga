d0b92035621aa03d9767e0da58ef40d6acea7147
Edgar Riba <edgar.riba@gmail.com>
2022-12-21 16:59:51 +0100

    remove unsupported services (#56)

--- py/tests/test_package.py
+++ py/tests/test_package.py
@@ -13,9 +13,7 @@
 # limitations under the License.
 import farm_ng
 from farm_ng.canbus import canbus_pb2
-from farm_ng.controller import controller_pb2
 from farm_ng.oak import oak_pb2
-from farm_ng.state_estimator import state_estimator_pb2
 
 
 def test_import() -> None:
@@ -25,6 +23,4 @@ def test_import() -> None:
     assert farm_ng.oak.__version__ is not None
 
     assert canbus_pb2 is not None
-    assert controller_pb2 is not None
     assert oak_pb2 is not None
-    assert state_estimator_pb2 is not None
