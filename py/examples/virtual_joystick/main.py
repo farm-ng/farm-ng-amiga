2f78671917b32d2af6a1d9e8d116dce69976cf85
Kyle Coble <kwc57@cornell.edu>
2023-01-17 17:23:16 -0500

    bi-directional stream for sending CAN msgs used in virtual joystick

--- py/examples/virtual_joystick/main.py
+++ py/examples/virtual_joystick/main.py
@@ -269,7 +269,7 @@ class VirtualJoystickApp(App):
 
         # Canbus task(s)
         self.async_tasks.append(asyncio.ensure_future(self.stream_canbus(canbus_client)))
-        canbus_client.stub.sendCanbusMessage(self.pose_generator())
+        self.async_tasks.append(asyncio.ensure_future(self.send_can_msgs(canbus_client)))
 
         # Drawing task(s)
         self.async_tasks.append(asyncio.ensure_future(self.draw()))
@@ -368,6 +368,42 @@ class VirtualJoystickApp(App):
                 except Exception as e:
                     print(e)
 
+    async def send_can_msgs(self, client: CanbusClient) -> None:
+        """This task ensures the canbus client sendCanbusMessage method has the pose_generator it will use to send
+        messages on the CAN bus to control the Amiga robot."""
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
+        response_stream = None
+        while True:
+            # check the state of the service
+            state = await client.get_state()
+
+            # Wait for a running CAN bus service
+            if state.value != service_pb2.ServiceState.RUNNING:
+                # Cancel existing stream, if it exists
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None
+                print("Waiting for running canbus service...")
+                await asyncio.sleep(0.1)
+                continue
+
+            if response_stream is None:
+                print("Start sending CAN messages")
+                response_stream = client.stub.sendCanbusMessage(self.pose_generator())
+
+            try:
+                async for response in response_stream:
+                    # Sit in this loop and wait until canbus service reports back it is not sending
+                    assert response.success
+            except Exception as e:
+                print(e)
+                response_stream.cancel()
+                response_stream = None
+                continue
+
+            await asyncio.sleep(0.1)
 
     async def pose_generator(self, period: float = 0.02):
         """The pose generator yields an AmigaRpdo1 (auto control command) for the canbus client to send on the bus
