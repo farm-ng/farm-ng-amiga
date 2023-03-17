7d2b229a4e9329f88e7670ad8aa327e9d778c2d2
Edgar Riba <edgar.riba@gmail.com>
2023-03-17 11:02:46 +0100

    fix people_detection packaging (#96)

--- py/farm_ng/people_detection/__init__.py
+++ py/farm_ng/people_detection/__init__.py
@@ -11,18 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import farm_ng
-from farm_ng.canbus import canbus_pb2
-from farm_ng.oak import oak_pb2
-from farm_ng.people_detection import people_detection_pb2
-
-
-def test_import() -> None:
-    assert farm_ng.core is not None
-    assert farm_ng.core.__version__ is not None
-    assert farm_ng.oak is not None
-    assert farm_ng.oak.__version__ is not None
-
-    assert canbus_pb2 is not None
-    assert people_detection_pb2 is not None
-    assert oak_pb2 is not None
