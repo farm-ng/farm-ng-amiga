37c30748feaee9d3f818a977455d6e8dd4b7beac
edgarriba <edgar.riba@gmail.com>
2023-09-25 13:25:46 +0000

    update camera example

--- py/examples/camera_client/main.py
+++ py/examples/camera_client/main.py
@@ -20,6 +20,7 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+from kornia_rs import ImageDecoder
 from farm_ng.core.event_client import EventClient
 from farm_ng.core.event_service_pb2 import EventServiceConfig
 from farm_ng.core.events_file_reader import proto_from_json_file
@@ -34,15 +35,16 @@ async def main(service_config_path: Path) -> None:
     # create a client to the camera service
     config: EventServiceConfig = proto_from_json_file(service_config_path, EventServiceConfig())
 
+    # instantiate the image decoder
+    image_decoder = ImageDecoder()
+
     async for event, message in EventClient(config).subscribe(config.subscriptions[0], decode=True):
         print(f"Timestamps: {event.timestamps[-2]}")
         print(f"Meta: {message.meta}")
         print("###################\n")
 
         # cast image data bytes to numpy and decode
-        # NOTE: explore frame.[rgb, disparity, left, right]
-        image = np.frombuffer(message.image_data, dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+        image = np.from_dlpack(image_decoder.decode(message.image_data))
 
         # visualize the image
         cv2.namedWindow("image", cv2.WINDOW_NORMAL)
