b9fa3b2dd952f76c2318bcae295c773287c43e89
Guilherme <39603677+guilhermedemouraa@users.noreply.github.com>
2023-09-28 15:09:39 -0700

    GPS playback example (#141)

---
+++ py/examples/file_reader_gps/README.md
@@ -0,0 +1,3 @@
+# Amiga log file reader GPS example
+
+URL: https://amiga.farm-ng.com/docs/examples/file_reader_gps/
---
+++ py/examples/file_reader_gps/main.py
@@ -0,0 +1,84 @@
+# Copyright (c) farm-ng, inc.
+#
+# Licensed under the Amiga Development Kit License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/farm-ng/amiga-dev-kit/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import argparse
+
+from farm_ng.core.events_file_reader import build_events_dict
+from farm_ng.core.events_file_reader import EventLogPosition
+from farm_ng.core.events_file_reader import EventsFileReader
+from farm_ng.gps import gps_pb2
+
+
+def main(file_name: str, msg_type: str) -> None:
+    if msg_type not in ["relposned", "pvt"]:
+        raise RuntimeError(f"Message type not recognized: {msg_type}")
+
+    # create the file reader
+    reader = EventsFileReader(file_name)
+    success: bool = reader.open()
+    if not success:
+        raise RuntimeError(f"Failed to open events file: {file_name}")
+
+    # get the index of the events file
+    events_index: list[EventLogPosition] = reader.get_index()
+
+    # structure the index as a dictionary of lists of events
+    events_dict: dict[str, list[EventLogPosition]] = build_events_dict(events_index)
+    print(f"All available topics: {sorted(events_dict.keys())}")
+
+    gps_events = events_dict[f"/gps/{msg_type}"]
+    print(f"Found {len(gps_events)} packets of gps/{msg_type}\n")
+
+    for event_log in gps_events:
+
+        # parse the message
+        msg: gps_pb2.RelativePositionFrame | gps_pb2.GpsFrame = event_log.read_message()
+
+        if msg_type == "pvt":
+            print(f"Message stamp: {msg.stamp.stamp}")
+            print(f"GPS time: {msg.gps_time.stamp}")
+            print(f"Latitude: {msg.latitude}")
+            print(f"Longitude: {msg.longitude}")
+            print(f"Altitude: {msg.altitude}")
+            print(f"Ground speed: {msg.ground_speed}")
+            print(f"Speed accuracy: {msg.speed_accuracy}")
+            print(f"Horizontal accuracy: {msg.horizontal_accuracy}")
+            print(f"Vertical accuracy: {msg.vertical_accuracy}")
+            print(f"P dop: {msg.p_dop}\n################################")
+
+        elif msg_type == "relposned":
+            print(f"Message stamp: {msg.stamp.stamp}")
+            print(f"GPS time: {msg.gps_time.stamp}")
+            print(f"Relative pose north: {msg.relative_pose_north}")
+            print(f"Relative pose east: {msg.relative_pose_east}")
+            print(f"Relative pose down: {msg.relative_pose_down}")
+            print(f"Relative pose length: {msg.relative_pose_length}")
+            print(f"Accuracy north: {msg.accuracy_north}")
+            print(f"Accuracy east: {msg.accuracy_east}")
+            print(f"Accuracy down: {msg.accuracy_down}")
+            print(f"Carrier solution: {msg.carr_soln}")
+            print(f"GNSS fix ok: {msg.gnss_fix_ok}\n################################")
+
+    assert reader.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Event file reader example for parsing GPS messages.")
+    parser.add_argument("--file-name", type=str, required=True, help="Path to the `events.bin` file.")
+    parser.add_argument(
+        "--msg-type", type=str, default="relposned", help="The name of the gps interface to read: relposned or pvt."
+    )
+    args = parser.parse_args()
+    main(args.file_name, args.msg_type)
