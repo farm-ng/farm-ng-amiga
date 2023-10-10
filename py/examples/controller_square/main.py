252e5c402eee648f1b7646ef7e625902005d6c24
Kyle Coble <kwc57@cornell.edu>
2023-10-10 17:04:30 -0400

    Split the paths into segments

--- py/examples/controller_square/main.py
+++ py/examples/controller_square/main.py
@@ -68,12 +68,14 @@ async def follow_track(service_config: EventServiceConfig) -> None:
     await EventClient(service_config).request_reply("/follow_track", StringValue(value="my_custom_track"))
 
 
-async def build_square(service_config: EventServiceConfig, side_length: float) -> FilterTrack:
+async def build_square(service_config: EventServiceConfig, side_length: float, clockwise: bool) -> FilterTrack:
     """Build a square track, from the current pose of the robot.
 
     Args:
         service_config (EventServiceConfig): The controller service config.
         side_length (float): The side length of the square.
+        clockwise (bool): True will drive the square clockwise (right hand turns).
+                        False is counter-clockwise (left hand turns).
     """
 
     # Query the controller for the current pose of the robot in the world frame
@@ -82,62 +84,116 @@ async def build_square(service_config: EventServiceConfig, side_length: float) -
     # Create a container to store the track waypoints
     track_waypoints: list[Pose3F64] = []
 
+    angle: float = radians(-90) if clockwise else radians(90)
+
     # Add the first goal at the current pose of the robot
     world_pose_goal0: Pose3F64 = world_pose_robot * Pose3F64(a_from_b=Isometry3F64(), frame_a="robot", frame_b="goal0")
     track_waypoints.append(world_pose_goal0)
 
     # Drive forward 1 meter (first side of the square)
-    track_waypoints.append(create_straight_segment(track_waypoints[-1], "goal1", side_length))
+    track_waypoints.extend(create_straight_segment(track_waypoints[-1], "goal1", side_length))
 
     # Turn left 90 degrees (first turn)
-    track_waypoints.append(create_turn_segment(track_waypoints[-1], "goal2", 90))
+    track_waypoints.extend(create_turn_segment(track_waypoints[-1], "goal2", angle))
 
     # Add second side and turn
-    track_waypoints.append(create_straight_segment(track_waypoints[-1], "goal3", side_length))
-    track_waypoints.append(create_turn_segment(track_waypoints[-1], "goal4", 90))
+    track_waypoints.extend(create_straight_segment(track_waypoints[-1], "goal3", side_length))
+    track_waypoints.extend(create_turn_segment(track_waypoints[-1], "goal4", angle))
 
-    # Add third side and turn
-    track_waypoints.append(create_straight_segment(track_waypoints[-1], "goal5", side_length))
-    track_waypoints.append(create_turn_segment(track_waypoints[-1], "goal6", 90))
+    # # Add third side and turn
+    track_waypoints.extend(create_straight_segment(track_waypoints[-1], "goal5", side_length))
+    track_waypoints.extend(create_turn_segment(track_waypoints[-1], "goal6", angle))
 
-    # Add fourth side and turn
-    track_waypoints.append(create_straight_segment(track_waypoints[-1], "goal7", side_length))
-    track_waypoints.append(create_turn_segment(track_waypoints[-1], "goal8", 90))
+    # # Add fourth side and turn
+    track_waypoints.extend(create_straight_segment(track_waypoints[-1], "goal7", side_length))
+    track_waypoints.extend(create_turn_segment(track_waypoints[-1], "goal8", angle))
 
     # Return the list of waypoints as a FilterTrack proto message
     # This is the format currently expected by the controller service
     return format_track(track_waypoints)
 
 
-def create_straight_segment(previous_pose: Pose3F64, next_frame_b: str, distance: float) -> Pose3F64:
+def create_straight_segment(
+    previous_pose: Pose3F64, next_frame_b: str, distance: float, spacing: float = 0.1
+) -> list[Pose3F64]:
     """Compute a straight segment of a square.
 
     Args:
         previous_pose (Pose3F64): The previous pose.
         next_frame_b (str): The name of the child frame of the next pose.
         distance (float): The side length of the square.
+        spacing (float): The spacing between waypoints, in meters.
 
     Returns:
-        Pose3F64: The next pose.
+        Pose3F64: The poses of the straight segment.
     """
-    straight_segment: Pose3F64 = Pose3F64(
-        a_from_b=Isometry3F64([distance, 0, 0], Rotation3F64.Rz(0)), frame_a=previous_pose.frame_b, frame_b=next_frame_b
-    )
-    return previous_pose * straight_segment
+    # Create a container to store the track segment waypoints
+    segment_poses: list[Pose3F64] = [previous_pose]
+
+    # For tracking the number of segments and remaining angle
+    counter: int = 0
+    remaining_distance: float = distance
+
+    while abs(remaining_distance) > 0.01:
+        # Compute the distance of the next segment
+        segment_distance: float = min(remaining_distance, spacing)
+
+        # Compute the next pose
+        straight_segment: Pose3F64 = Pose3F64(
+            a_from_b=Isometry3F64([segment_distance, 0, 0], Rotation3F64.Rz(0)),
+            frame_a=segment_poses[-1].frame_b,
+            frame_b=f"{next_frame_b}_{counter}",
+        )
+        segment_poses.append(segment_poses[-1] * straight_segment)
+
+        # Update the counter and remaining angle
+        counter += 1
+        remaining_distance -= segment_distance
+
+    # Rename the last pose to the desired name
+    segment_poses[-1].frame_b = next_frame_b
+    return segment_poses
 
 
-def create_turn_segment(previous_pose: Pose3F64, next_frame_b: str, angle: float) -> Pose3F64:
+def create_turn_segment(
+    previous_pose: Pose3F64, next_frame_b: str, angle: float, spacing: float = 0.1
+) -> list[Pose3F64]:
     """Compute a turn segment of a square.
 
     Args:
         previous_pose (Pose3F64): The previous pose.
         next_frame_b (str): The name of the child frame of the next pose.
-        angle (float): The angle to turn, in degrees (+ left, - right).
+        angle (float): The angle to turn, in radians (+ left, - right).
+        spacing (float): The spacing between waypoints, in radians.
+    Returns:
+        list[Pose3F64]: The poses of the turn segment.
     """
-    turn_segment: Pose3F64 = Pose3F64(
-        a_from_b=Isometry3F64.Rz(radians(angle)), frame_a=previous_pose.frame_b, frame_b=next_frame_b
-    )
-    return previous_pose * turn_segment
+    # Create a container to store the track segment waypoints
+    segment_poses: list[Pose3F64] = [previous_pose]
+
+    # For tracking the number of segments and remaining angle
+    counter: int = 0
+    remaining_angle: float = angle
+
+    while abs(remaining_angle) > 0.01:
+        # Compute the angle of the next segment
+        segment_angle: float = min(remaining_angle, spacing)
+
+        # Compute the next pose
+        turn_segment: Pose3F64 = Pose3F64(
+            a_from_b=Isometry3F64.Rz(segment_angle),
+            frame_a=segment_poses[-1].frame_b,
+            frame_b=f"{next_frame_b}_{counter}",
+        )
+        segment_poses.append(segment_poses[-1] * turn_segment)
+
+        # Update the counter and remaining angle
+        counter += 1
+        remaining_angle -= segment_angle
+
+    # Rename the last pose to the desired name
+    segment_poses[-1].frame_b = next_frame_b
+    return segment_poses
 
 
 def format_track(track_waypoints: list[Pose3F64]) -> FilterTrack:
