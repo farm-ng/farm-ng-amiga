43d35996838980309d5d965f9f4fc8ae13400158
Ian B <ian@tesseraktinstitute.org>
2024-07-24 20:58:13 +0000

    initial

---
+++ py/examples/recorder_annotation_client/main.py
@@ -0,0 +1,42 @@
+# Copyright (c) farm-ng, inc.
+#
+# Licensed under the Amiga Development Kit License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/farm-ng/amiga-dev-kit/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import asyncio
+from pathlib import Path
+
+from farm_ng.core.event_client import EventClient
+from farm_ng.core.event_service_pb2 import EventServiceConfig
+from farm_ng.core.events_file_reader import proto_from_json_file
+from google.protobuf.struct_pb2 import Struct
+
+
+async def main(service_config_path: Path) -> None:
+    """Run the camera service client.
+
+    Args:
+        service_config_path (Path): The path to the camera service config.
+    """
+    # create a client to the camera service
+    config: EventServiceConfig = proto_from_json_file(service_config_path, EventServiceConfig())
+
+    async for event, message in EventClient(config).subscribe(config.subscriptions[0], decode=False):
+        print(message)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="python main.py", description="Stream motor states from the canbus service.")
+    parser.add_argument("--service-config", type=Path, required=True, help="The camera config.")
+    args = parser.parse_args()
+
+    asyncio.run(main(args.service_config))
